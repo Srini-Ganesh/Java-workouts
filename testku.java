8b47ae2db5ffc6cee88f80431b449778dbac1d53
Ganesan s <139910547+Srini-Ganesh@users.noreply.github.com>
2023-10-13 19:58:27 +0530

    Update PowerOfTwo.java

--- testku.java
+++ testku.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class powerOfTwo
+class PowerOfTwo
 {
 	public static void main(String args[])
 	{
@@ -7,10 +7,12 @@ class powerOfTwo
 		System.out.print("Enter a number: ");
 		int num =read.nextInt();
 		int n=num;
+		//divided the num untill no longer divided by two
 		while(num%2==0)
 		{
 			num/=2;
 		}
+		// 1 means it completly divided by two
 		if(num==1)
 		{
 			System.out.println(n+" is a power of 2.");
