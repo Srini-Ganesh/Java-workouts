6eaa82f33db42227edb2b15618e02334fe23b2d3
Ganesan s <139910547+Srini-Ganesh@users.noreply.github.com>
2023-10-18 14:54:43 +0530

    Add files via upload

---
+++ testku.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+import java.sql.*;
+class testku
+{
+	public static void main(String args[])
+	{
+		Scanner read=new Scanner(System.in);
+		System.out.print("Enter year to check: ");
+		int year=read.nextInt();
+		
+		if(year%400==0)
+		{
+			System.out.println(year+" is a leap year..");
+		}
+		else if(year%4==0 && year%100!=0)
+		{
+			System.out.println(year+" is a leap year..");
+		}
+		else
+		{
+			System.out.println(year+" is not a leap year..");
+		}
+	}
+}
