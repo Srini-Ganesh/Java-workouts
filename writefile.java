b5807bdddf947092df94d2f2ed74f4e077f772e9
Ganesan s <139910547+Srini-Ganesh@users.noreply.github.com>
2023-10-22 19:54:06 +0530

    Add files via upload

---
+++ writefile.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.io.IOException;
+class writefile
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			FileWriter fw=new FileWriter("newfile.txt");
+			fw.write("hello ganesh");
+			fw.close();
+			System.out.println("written successfully..");
+		}
+		catch(IOException e)
+		{
+			System.out.print("error occured: ");
+			e.printStackTrace();
+		}
+	}
+}
