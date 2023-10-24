df4eb50f2dd54e4d02ee4f2791140580e34db694
Ganesan s <139910547+Srini-Ganesh@users.noreply.github.com>
2023-10-24 23:43:41 +0530

    Add files via upload

---
+++ NumOfCharInString.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class check
+{
+	public static int numberOfCharacter(String s,char c)
+	{
+		int count=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)==c || s.charAt(i)==Character.toUpperCase(c))
+				count++;
+		}
+		return count;
+	}
+}
+class NumOfCharInString
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a String:");
+		String str=sc.nextLine();
+		System.out.print("Enter a character to check:");
+		String strch = sc.nextLine();
+		char ch = strch.charAt(0);
+		int count = check.numberOfCharacter(str,ch);
+		System.out.println("There are "+count+" \'"+ch+"\' in "+str);
+	}
+}
+		
+		
