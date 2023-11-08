10de3fd60f7cf5adfa2800e32898abb07575bf13
Ganesan s <139910547+Srini-Ganesh@users.noreply.github.com>
2023-11-08 17:11:22 +0530

    Add files via upload

---
+++ testku.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.time.*;
+import java.sql.*;
+class testku
+{
+	public static int sumOfDigits(int num)
+	{
+		int total=0;
+		while(num>0)
+		{
+			total+=num%10;
+			num=num/10;
+		}
+		return total;
+	}
+	public static void main(String args[])
+	{
+		Scanner read=new Scanner(System.in);
+		System.out.print("Enter the number: ");
+		int num=read.nextInt();
+		System.out.print("\nSum of digits: "+sumOfDigits(num)+"\n");
+	}
+}
